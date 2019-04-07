efa30f23aaf42d18ae22d8a80a5e0545b1c60d80
Saber0w0 <MrOdaNobunaga@gmail.com>
2019-04-08 00:30:50 +1000

    去掉exception

--- practice/MultiCore.py
+++ practice/MultiCore.py
@@ -1,6 +1,6 @@
 import mpi4py.MPI as MPI
 import json
-from contextlib import suppress
+# from contextlib import suppress
 from CheckLocation import checkLocation
 from printTwitterNumber import printTwitterNumber
 from printTagNumber import printTagNumber
@@ -50,10 +50,10 @@ def searchTags(text, location):
 
 if comm_rank == 0:
     start_time = time.time()
-    with open("resources/tinyTwitter(3).json") as twitter:
+    with open("ccc/practice/resources/bigTwitter.json", "r") as twitter:
         for line in twitter:
             package.append(line)
-            if len(package) == 10:
+            if len(package) == 15:
                 comm.send(package, dest=rank1)
                 rank1 = rank1 + 1
                 if rank1 == comm_size:
@@ -71,21 +71,22 @@ if comm_rank > 0:
         data_recv = comm.recv(source=0)
         if data_recv == False:
             break
-        with suppress(Exception):
-            for info in data_recv:
-                if info[2: 4] != "id":
-                    continue
-                if info[-2] == ",":
-                    info = info[:-2]
-                info_json = json.loads(info)
-                x = info_json["doc"]["coordinates"]["coordinates"][0]
-                y = info_json["doc"]["coordinates"]["coordinates"][1]
-                text = info_json["doc"]["text"]
-                if x and y:
-                    location = checkLocation.getLocation(x, y)
-                    if location:
-                        numberCounter.append(location)
-                        searchTags(text, location)
+        # with suppress(Exception):
+        for info in data_recv:
+            if info[2: 4] != "id":
+                continue
+            if info[-2] == ",":
+                info = info[:-2]
+            info_json = json.loads(info)
+            x = info_json["doc"]["coordinates"]["coordinates"][0]
+            y = info_json["doc"]["coordinates"]["coordinates"][1]
+            text = info_json["doc"]["text"]
+            if x and y:
+                location = checkLocation.getLocation(x, y)
+                if location:
+                    numberCounter.append(location)
+                    searchTags(text, location)
+
 
 newNumberCounter = comm.gather(numberCounter, root=0)
 newTags_dict = comm.gather(tags_dict, root=0)
